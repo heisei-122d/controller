9c28ad5187274fc5288ef31647fe895f5b500848
Tari Tari <makatu1999@icloud.com>
2019-04-25 00:06:14 +0900

    Create define_button.h

---
+++ sketch_apr22c_EOH_SEND/define_button.h
@@ -0,0 +1,22 @@
+/* define_button.h
+ * K.A.I. T A K U B O K U
+ * 
+ * 25 APRIL 31 HEISEI
+ *
+ *
+ *
+ *
+ *
+ */
+
+#define BUTTON_01
+#define BUTTON_02
+#define BUTTON_03
+#define BUTTON_04
+#define BUTTON_05
+#define BUTTON_06
+#define BUTTON_07
+#define BUTTON_08
+#define BUTTON_09
+#define BUTTON_10
+
